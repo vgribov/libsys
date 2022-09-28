f38521b5865fd7f249ded038e99d78c0fd47a2ac
Seva Gribov <seva.gribov@aerq.com>
2022-09-28 16:15:36 +0200

    mmap: Add mmap/munmap wrappers

---
+++ mmap.hpp
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <iostream>
+#include <utility>
+#include <sys/mman.h>
+
+#include "file.hpp"
+#include "utils.hpp"
+
+namespace sys {
+
+struct Mmap_error : public Error {};
+
+struct Mem_map {
+    void*  data;
+    size_t length;
+};
+
+inline utils::Ptr<Mem_map>
+mmap(void* addr, size_t length, int prot, int flags, File_des fd, off_t offset)
+{
+    void* p = ::mmap(addr, length, prot, flags, fd.get(), offset);
+    if (p == MAP_FAILED) throw Error{};
+    auto deleter = [](Mem_map* mm) {
+        ::munmap(mm->data, mm->length);
+        delete mm;
+    };
+    return { new Mem_map{p, length}, deleter };
+}
+
+} // namespace sys
